1ca319e92af2249816497fad79c0160fc091004e
Pedro Dias <pedrodiasfonseca@gmail.com>
2016-04-04 18:17:08 -0300

    Add offset menu to mobile view and smooth scroll to anchor links

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,11 @@
 var canvas, stage, exportRoot;
 function init() {
-    // Load svg animation
+    var stage = loadSVGAnimation();
+    var snapper = loadOffsetMenu();
+    var scroller = loadAnchorScrollNavigation(snapper.close);
+}
+
+function loadSVGAnimation() {
     canvas = document.getElementById("canvas");
     exportRoot = new lib.animacaoblogueiro();
 
@@ -11,8 +16,46 @@ function init() {
     createjs.Ticker.setFPS(lib.properties.fps);
     createjs.Ticker.addEventListener("tick", stage);
 
-    var swiper = new Swiper('.slider', {
-        pagination: '.slider__dots',
-        paginationClickable: true
+    return stage;
+}
+
+function loadOffsetMenu() {
+    var snapper = new Snap({
+        element: document.getElementById('menu__content'),
+        touchToDrag: false
     });
+
+    snapper.on('open', function() {
+        return $('.menu__dragger use')[0]
+            .setAttribute('xlink:href', 'images/sprite.svg#arrow-left');
+    });
+
+    snapper.on('close', function() {
+        return $('.menu__dragger use')[0]
+            .setAttribute('xlink:href', 'images/sprite.svg#menu');
+    });
+
+    $('.menu__dragger').on('click', function() {
+        var state = snapper.state().state;
+        return state === 'closed' ? snapper.open('right') : snapper.close();
+    });
+
+    return snapper;
+}
+
+function loadAnchorScrollNavigation(callback) {
+    var scrollableArea = document.getElementById('menu__content');
+    var scroller = zenscroll.createScroller(scrollableArea, 2000, 0);
+
+    $('[data-anchor-link]').on('click', function(event) {
+        var hasHref = event.target.hasAttribute('href');
+        var target = hasHref ? event.target.getAttribute('href') : '';
+        target = target.replace('#', '');
+        var elmTarget = document.getElementById(target);
+        if (!elmTarget) { return null; }
+        scroller.to(elmTarget);
+        callback();
+    });
+
+    return scroller;
 }
